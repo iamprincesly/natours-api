cff88f40746d9447ac6c27fc2320f61edc070e12
iamprincesly <iamprincesly@gmail.com>
2021-09-22 02:23:56 +0100

    Final clean up

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -94,7 +94,14 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
     const tourIDs = bookings.map((el) => el.tour);
     const tours = await Tour.find({ _id: { $in: tourIDs } });
 
-    res.status(200).render('overview', { title: 'My tours', tours });
+    res.status(200)
+        .set(
+            'Content-Security-Policy',
+            `default-src 'self' ${req.protocol}://${req.get(
+                'host'
+            )} https://*.mapbox.com https://*.stripe.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://js.stripe.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;`
+        )
+        .render('overview', { title: 'My tours', tours });
 });
 
 exports.updateUserData = catchAsync(async (req, res, next) => {
