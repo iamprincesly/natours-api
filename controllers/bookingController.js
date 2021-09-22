426a317536e2dfb9bd0b85c18ad2c806d0ea75b1
iamprincesly <iamprincesly@gmail.com>
2021-09-22 01:35:32 +0100

    Fixed stripe error 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -72,8 +72,7 @@ exports.deleteBooking = deleteOne(Booking);
 
 const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id;
-    const user = (await User.find({ email: session.customer_details.email }))
-        .id;
+    const user = (await User.find({ email: session.customer_email })).id;
     const price = session.amount_total / 100;
     await Booking.create({ tour, user, price });
 };
